16117a0014a18ae8523e8bb38483742aeaed0812
Han-Yu Chen <okashi0083.tem97@g2.nctu.edu.tw>
2016-01-02 11:14:58 +0800

    Create Telephone.js

---
+++ Telephone.js
@@ -0,0 +1,12 @@
+var a ='Store #6354 1135 N.W. LEARY WAY 14 AVE NW & NW LEARY WAY BALLARD, WA 98107 (206) 789-8839 Get store hours and directions'
+
+
+function transform(data) {
+    if (data != null) {
+        var n1 = data.trim();
+      	return n1.match(/\(\d+\)\s\d+\-\d+/).toString();
+    }
+}
+
+
+transform(a);
