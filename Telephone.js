e581851fd9ad6a449c8e3227d9460c467b6cfcb8
okashi0083 <okashi0083@hotmail.com>
2016-02-29 16:46:54 +0800

    Update Telephone.js

--- Telephone.js
+++ Telephone.js
@@ -1,4 +1,4 @@
-/* extract telephone number */
+/* extract telephone number by javascript*/
 
 var a ='Store #6354 1135 N.W. LEARY WAY 14 AVE NW & NW LEARY WAY BALLARD, WA 98107 (206) 789-8839 Get store hours and directions'
 
